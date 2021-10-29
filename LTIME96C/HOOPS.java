74b77c8ca170de6bdddd46bf6a364f6a6b6656ec
Pratham Shroff <pshroff@bu.edu>
2021-10-28 23:03:24 -0400

    Create HOOPS.java

---
+++ LTIME96C/HOOPS.java
@@ -0,0 +1,11 @@
+import java.util.*; import java.lang.*; import java.io.*;
+
+class Codechef {
+	public static void main (String[] args) throws java.lang.Exception {
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		while(t-- > 0) {
+		    System.out.println((sc.nextInt() + 1) / 2);
+		}
+	}
+}
